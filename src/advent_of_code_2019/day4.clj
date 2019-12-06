228ce30f51a5a038366071ae80182128bab3de76
Derek Slager <derekslager@gmail.com>
2019-12-05 19:19:44 -0500

    Prettier Day 4.

--- src/advent_of_code_2019/day4.clj
+++ src/advent_of_code_2019/day4.clj
@@ -1,22 +1,22 @@
 (ns advent-of-code-2019.day4)
 
-(def input (range 278384 (inc 824795)))
+(def input (->> (range 278384 (inc 824795)) (map str)))
 
 ;;; part 1
 (defn is-pw [n]
-  (let [pairs (partition 2 1 (str n))]
+  (let [pairs (partition 2 1 n)]
     (and (some (partial apply =) pairs)
-         (every? (fn [[l r]] (<= (int l) (int r))) pairs))))
+         (apply <= (map int n)))))
 
 (->> input (filter is-pw) count)
 
 ;;; part 2
 (defn is-pw-2 [n]
-  (and (is-pw n)
-       (some (partial = 2) (->> (frequencies (str n)) vals))))
+  (and (apply <= (map int n))
+       (some (partial = 2) (->> (frequencies n) vals))))
 
-#_(is-pw-2 112233)
-#_(is-pw-2 123444)
-#_(is-pw-2 111122)
+#_(is-pw-2 "112233")
+#_(is-pw-2 "123444")
+#_(is-pw-2 "111122")
 
 (->> input (filter is-pw-2) count)
