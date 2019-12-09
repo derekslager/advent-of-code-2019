51a9116308350956740556938e0d699fdccb7d5e
Derek Slager <derekslager@gmail.com>
2019-12-08 23:23:40 -0800

    Refactoring intcode.

--- src/advent_of_code_2019/day7.clj
+++ src/advent_of_code_2019/day7.clj
@@ -1,5 +1,5 @@
 (ns advent-of-code-2019.day7
-  (:require [advent-of-code-2019.day5 :as day5]
+  (:require [advent-of-code-2019.intcode :as intcode]
             [clojure.math.combinatorics :as combo]
             [advent-of-code-2019.util :as util]
             [clojure.core.async :as async]))
@@ -13,7 +13,7 @@
 (->> (for [settings (phase-settings 0 5)]
        (reduce
         (fn [last-output phase-setting]
-          (day5/compute input [phase-setting last-output] "day7/part1"))
+          (intcode/compute input [phase-setting last-output] "day7/part1"))
         0
         settings))
      (apply max))
@@ -31,7 +31,7 @@
            (async/>!! first-ch 0)
            (doseq [index (range 5)]
              (async/pipe
-              (day5/compute-async input (nth input-chs index) (nth result-chs index) (str "day7/amp" index))
+              (intcode/compute-async input (nth input-chs index) (nth result-chs index) (str "day7/amp" index))
               (nth input-chs (mod (inc index) 5))))
 
            (async/<!! (nth result-chs 4)))))
---
+++ src/advent_of_code_2019/intcode.clj
@@ -0,0 +1,65 @@
+(ns advent-of-code-2019.intcode
+  (:require [clojure.core.async :as async]
+            [clojure.string :as str]
+            [clojure.tools.logging :as log]))
+
+(defn compute-async [ops input-ch result-ch & [id]]
+  (let [output-ch (async/chan)]
+    (async/go-loop [{:keys [pos state output] :as ctx} {:pos 0, :state ops}]
+      (let [code (nth state pos)
+            [_ pmodes op-code] (re-matches #"(\d*)(\d{2})" (str code))
+            op (if op-code (Integer/parseInt op-code) code)
+            modes (if-not (str/blank? pmodes)
+                    (map (fn [p]
+                           (condp = p
+                             \0 #(get state %)
+                             \1 (fn [x] x)))
+                         (reverse (format "%03d" (Integer/parseInt pmodes))))
+                    (repeat #(get state %)))
+            praw (fn [index] (get state (+ 1 index pos)))
+            pval (fn [index] ((nth modes index) (praw index)))]
+        (condp contains? op
+          #{99}
+          (do
+            (log/info id "halted!" output)
+            (async/>! result-ch output)
+            (async/close! result-ch))
+          #{1 2}
+          (recur (-> (update ctx :pos + 4)
+                     (update :state assoc (praw 2) ((case op 1 + 2 *) (pval 0) (pval 1)))))
+          #{3}
+          (do
+            (log/info id "reading next input value")
+            (let [next-input-value (async/<! input-ch)]
+              (log/info id "read:" next-input-value)
+              (recur (-> (update ctx :pos + 2)
+                         (update :state assoc (praw 0) next-input-value)))))
+          #{4}
+          (let [output (pval 0)]
+            (log/info id "put:" output)
+            (async/put! output-ch output)
+            (recur (-> (update ctx :pos + 2)
+                       (assoc :output output))))
+          #{5 6}
+          (recur (assoc ctx :pos
+                        (if ((case op 5 #(not= 0 %) 6 #(= 0 %)) (pval 0))
+                          (pval 1)
+                          (+ pos 3))))
+          #{7}
+          (recur (-> (update ctx :pos + 4)
+                     (update :state assoc (praw 2) (if (< (pval 0) (pval 1)) 1 0))))
+          #{8}
+          (recur (-> (update ctx :pos + 4)
+                     (update :state assoc (praw 2) (if (= (pval 0) (pval 1)) 1 0)))))))
+    output-ch))
+
+(defn fixed-inputs [inputs]
+  (let [ch (async/chan (async/buffer (count inputs)))]
+    (doseq [input inputs]
+      (async/>!! ch input))
+    ch))
+
+(defn compute [ops inputs & [id]]
+  (let [result-ch (async/chan)]
+    (compute-async ops (fixed-inputs inputs) result-ch id)
+    (async/<!! result-ch)))
